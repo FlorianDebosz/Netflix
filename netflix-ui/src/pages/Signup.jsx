4f89e2903e636ac8f157bf9bb5e1aa8be2137f4d
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-11-07 00:02:45 +0100

    🍱 ✨ Added Assets and Signup

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,5 +1,40 @@
 import React from 'react'
+import styled from "styled-components";
+import BackgroundImage from '../components/BackgroundImage';
+import Header from '../components/Header';
 
 export default function Signup() {
-    return <div>Signup</div>
+    return <Container>
+        <BackgroundImage />
+        <div className="content">
+        <Header />
+        <div className="body flex column a-center j-center">
+            <div className="text flex column">
+                <h1>Unlimited movies, TV shows and more</h1>
+                <h4>Watch anywhere. Cancel anytime</h4>
+                <h6>Ready to watch? Enter your email to create or restart membership</h6>
+            </div>
+             <div className="form">
+                <input type="email" placeholder="Email" name="email"/>
+                <input type="password" placeholder="Password" name="password"/>
+                <button>Get Started</button>
+             </div>
+             <button>Log in</button>
+        </div>
+        </div>
+    </Container>
 }
+
+const Container = styled.div`
+postion: relative;
+.content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+}
+`;
