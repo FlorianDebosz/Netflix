bd1478fce0b3efed39d3af801a213c7750e16c99
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-11-07 15:24:12 +0100

    🐛 Changed Text Not Available

--- netflix-ui/src/components/NotAvailable.jsx
+++ netflix-ui/src/components/NotAvailable.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function NotAvailable() {
   return (
-    <h1 className='notAvailable'> No Movies available for selected Genre</h1>
+    <h1 className='notAvailable'> Nothing available for selected Genre</h1>
   )
 }
